360282955e574689452307b46c4669290e5cddad
sajib689 <jibonraja11@gmail.com>
2024-06-24 14:25:10 +0600

    fix an error

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -1,5 +1,20 @@
+import Link from "next/link";
+import Image  from "next/image";
+import img from '../public/rounded-text.png'
 const ProjectsBtn = () => {
-  return <div>Project Btn</div>;
+  return (
+    <div className="mx-auto xl:mx-0 ">
+      <Link href={"/work"}>
+        <Image
+          src={img}
+          width={141}
+          height={148}
+          alt=""
+          className="animate-spin-slow w-full h-full"
+        />
+      </Link>
+    </div>
+  );
 };
 
 export default ProjectsBtn;
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,22 @@
+import ProjectsBtn from "./../components/ProjectsBtn";
 const Home = () => {
   return (
     <div className="bg-primary/60 h-full ">
-      <div className="">
-        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container">
+      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
+        <div className="text-center flex flex-col justify-center xl:pt-35 xl:text-left h-full container mx-auto">
           <h1 className="h1">
             Transforming Ideas <br /> Info{" "}
             <span className="text-accent">Digital Reality</span>
           </h1>
           <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-10 ">
-            As a MERN stack developer, I specialize in converting innovative
-            concepts into robust web applications. With expertise in MongoDB,
-            Express.js, React, and Node.js, I create dynamic, scalable, and
-            user-friendly solutions that meet modern digital demands. Explore my
-            portfolio to see how I bring ideas to life through clean code,
-            intuitive design, and seamless functionality.
+            I'm a MERN stack developer specializing in MongoDB, Express.js,
+            React, and Node.js. I create dynamic web applications that combine
+            clean code with intuitive design for seamless user experiences.
+            Explore my portfolio to see my work in action
           </p>
+          <div className="flex justify-center xl:hidden relative">
+            <ProjectsBtn />
+          </div>
         </div>
         <div></div>
       </div>
