92f03c92d9567185c78ca1deac0339277cab6cae
sajib689 <jibonraja11@gmail.com>
2024-06-24 17:37:29 +0600

    home page add owner image

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,10 @@
+import Image from "next/image";
+
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return <div className="hidden xl:flex xl:max-w-none">
+    <Image src={'/sajib1.png'} width={537} height={438} alt=""/>
+  </div>;
 };
 
 export default Avatar;
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,38 @@
 import ProjectsBtn from "./../components/ProjectsBtn";
+import { motion } from "framer-motion";
+import { fadeIn } from './../variants';
+import Avatar from './../components/Avatar';
 const Home = () => {
   return (
     <div className="bg-primary/60 h-full ">
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-35 xl:text-left h-full container mx-auto">
-          <h1 className="h1">
+          <motion.h1 variants={fadeIn('down',0.2)} initial='hidden' animate='show' exit='hidden' className="h1">
             Transforming Ideas <br /> Info{" "}
             <span className="text-accent">Digital Reality</span>
-          </h1>
-          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-10 ">
+          </motion.h1>
+          <motion.p variants={fadeIn('down',0.3)} initial='hidden' animate='show' exit='hidden' className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-10 ">
             I'm a MERN stack developer specializing in MongoDB, Express.js,
             React, and Node.js. I create dynamic web applications that combine
             clean code with intuitive design for seamless user experiences.
             Explore my portfolio to see my work in action
-          </p>
+          </motion.p>
           <div className="flex justify-center xl:hidden relative">
             <ProjectsBtn />
           </div>
+          <motion.div className="hidden xl:flex" variants={fadeIn('down',0.4)} initial='hidden' animate='show' exit='hidden'>
+            <ProjectsBtn />
+          </motion.div>
         </div>
-        <div></div>
+        <div className="w-[1200px] h-full absolute right-0 bottom-0">
+         <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0  ">
+
+         </div>
+         <div>Particles</div>
+         <motion.div variants={fadeIn('up',0.5)} transition={{duration: 1, ease: 'easeInOut'}} initial='hidden' animate='show' exit='hidden' className="w-full h-full max-w-[450px] max-h-[438px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]">
+          <Avatar/>
+         </motion.div>
+        </div>
       </div>
     </div>
   );
