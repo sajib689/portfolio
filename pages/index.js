542e1ed1ae86208297389642c85ed99273d889a4
sajib689 <jibonraja11@gmail.com>
2024-06-24 13:53:05 +0600

    home page design done

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-primary/60 h-full ">
+      <div className="">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container">
+          <h1 className="h1">
+            Transforming Ideas <br /> Info{" "}
+            <span className="text-accent">Digital Reality</span>
+          </h1>
+          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-10 ">
+            As a MERN stack developer, I specialize in converting innovative
+            concepts into robust web applications. With expertise in MongoDB,
+            Express.js, React, and Node.js, I create dynamic, scalable, and
+            user-friendly solutions that meet modern digital demands. Explore my
+            portfolio to see how I bring ideas to life through clean code,
+            intuitive design, and seamless functionality.
+          </p>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
