85eddcac3f0609b9336778dbd093512188e38b44
sajib689 <jibonraja11@gmail.com>
2024-06-25 10:33:06 +0600

    change the website name

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,10 @@ const Home = () => {
   return (
     <div className="bg-primary/60 h-full ">
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
-        <div className="text-center flex flex-col justify-center xl:pt-35 xl:text-left h-full container mx-auto">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           <motion.h1 variants={fadeIn('down',0.2)} initial='hidden' animate='show' exit='hidden' className="h1">
-            Transforming Ideas <br /> Info{" "}
-            <span className="text-accent">Digital Reality</span>
+            Frontend 
+            <span className="text-accent"> Developer</span>
           </motion.h1>
           <motion.p variants={fadeIn('down',0.3)} initial='hidden' animate='show' exit='hidden' className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-10 ">
             I'm a MERN stack developer specializing in MongoDB, Express.js,
@@ -29,7 +29,7 @@ const Home = () => {
          <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0  ">
 
          </div>
-         <ParticlesContainer/>
+         {/* <ParticlesContainer/> */}
          <motion.div variants={fadeIn('up',0.5)} transition={{duration: 1, ease: 'easeInOut'}} initial='hidden' animate='show' exit='hidden' className="w-full h-full max-w-[450px] max-h-[438px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]">
           <Avatar/>
          </motion.div>
