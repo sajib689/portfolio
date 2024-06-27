50ee3b0eab7bebedada3117ab662eceaa7adcd08
sajib689 <jibonraja11@gmail.com>
2024-06-27 17:58:41 +0600

    add about section

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,4 +1,6 @@
-// icons
+import { useState } from "react";
+import { motion } from "framer-motion";
+
 import {
   FaHtml5,
   FaCss3,
@@ -7,89 +9,54 @@ import {
   FaWordpress,
   FaFigma,
 } from "react-icons/fa";
-
 import {
   SiNextdotjs,
   SiFramer,
   SiAdobexd,
   SiAdobephotoshop,
 } from "react-icons/si";
-
-
-//  data
-const aboutData = [
-  {
-    title: 'skills',
-    info: [
-      {
-        title: 'Web Development',
-        icons: [
-          <FaHtml5 />,
-          <FaCss3 />,
-          <FaJs />,
-          <FaReact />,
-          <SiNextdotjs />,
-          <SiFramer />,
-          <FaWordpress />,
-        ],
-      },
-      {
-        title: 'UI/UX Design',
-        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
-      },
-    ],
-  },
-  {
-    title: 'awards',
-    info: [
-      {
-        title: 'Webby Awards - Honoree',
-        stage: '2011 - 2012',
-      },
-      {
-        title: 'Adobe Design Achievement Awards - Finalist',
-        stage: '2009 - 2010',
-      },
-    ],
-  },
-  {
-    title: 'experience',
-    info: [
-      {
-        title: 'UX/UI Designer - XYZ Company',
-        stage: '2012 - 2023',
-      },
-      {
-        title: 'Web Developer - ABC Agency',
-        stage: '2010 - 2012',
-      },
-      {
-        title: 'Intern - DEF Corporation',
-        stage: '2008 - 2010',
-      },
-    ],
-  },
-  {
-    title: 'credentials',
-    info: [
-      {
-        title: 'Web Development - ABC University, LA, CA',
-        stage: '2011',
-      },
-      {
-        title: 'Computer Science Diploma - AV Technical Institute',
-        stage: '2009',
-      },
-      {
-        title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
-        stage: '2006',
-      },
-    ],
-  },
-];
+import Circles from "./../../components/Circles";
+import Avatar from "./../../components/Avatar";
+import { fadeIn } from "./../../variants";
+import { TypeAnimation } from "react-type-animation";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="h-full bg-primary/30 py-16 text-center xl:text-left ">
+      <Circles />
+      <motion.div
+        variants={fadeIn("right", 0.2)}
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        className="hidden xl:flex absolute bottom-0 lg:left-[740px]"
+      >
+        <Avatar />
+      </motion.div>
+      <div className="container lg:w-[700px] lg:ms-20 mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
+        <div className="flex-1 flex flex-col justify-center">
+          <TypeAnimation
+            className="mt-12 text-4xl"
+            sequence={["About", 200, "About Me",700]}
+            style={{ fontSize: "2em" }}
+            repeat={Infinity}
+          />
+          <p className="mt-3">
+            Hello, I’m Sajib Hossen, a passionate frontend developer from
+            Jashore, Khulna, Bangladesh. Currently, I am pursuing a BA in
+            Islamic History and Culture at MM College, Jashore. With a keen eye
+            for design and a commitment to creating seamless user experiences, I
+            specialize in crafting visually appealing and highly functional web
+            interfaces. I have experience in frontend development with HTML,
+            CSS, JavaScript, Tailwind CSS, and React.js. My backend development
+            skills include Node.js and Express.js, and I manage databases using
+            MongoDB. For authentication and security, I use JWT, and I also work
+            with Firebase for various services. Welcome to my portfolio!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
